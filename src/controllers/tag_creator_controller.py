31cb1f05128b37d8b6f4deb9b541a71491ff33fc
AgathaGoncalves <71910283+AgathaGoncalves@users.noreply.github.com>
2024-02-08 02:01:39 -0300

    Test: Implementing unittests

--- src/controllers/tag_creator_controller.py
+++ src/controllers/tag_creator_controller.py
@@ -16,7 +16,7 @@ class TagCreatorController:
     def __format_response(self, path_from_tag : str)-> Dict:
         return{
             "data": {
-                "type":"Tag image",
+                "type": "Tag Image",
                 "count": 1,
                 "path": f'{path_from_tag}.png'
             }
