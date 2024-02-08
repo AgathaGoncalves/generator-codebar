8d352bcdd6a88b489a375637a4dd788d3962f464
AgathaGoncalves <71910283+AgathaGoncalves@users.noreply.github.com>
2024-02-07 23:59:17 -0300

    Feat: Adding drivers, controllers and code structuration

--- src/viwes/tag_creator_view.py
+++ src/viwes/tag_creator_view.py
@@ -1,14 +1,17 @@
 from src.viwes.http_types.http_request import  HttpRequest
 from src.viwes.http_types.http_response import  HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 
 class  TagCreatorView:
 
     def validate_and_create(self, http_request: HttpRequest):
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
-        print('To no arquivo tag_creator_view')
+        formatted_response = tag_creator_controller.create(product_code)
 
-        return HttpResponse(status_code=200,
-                            body={"message": f"Tag created for the product {product_code}"})
+
+        return HttpResponse(status_code = 200, body = formatted_response)
